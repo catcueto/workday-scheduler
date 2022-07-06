1260a7e9253e571b7cfdb935310c3fb32f7f938b
catcueto <catacueto@gmail.com>
2022-07-06 17:43:28 -0400

    added time slots and SAVE btn

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//PSEUDOCODE
+
+// STEP 1: After opening the planner, the correct day is displayed at the top (Thursday, September 2nd) --> uses time API
+
+// STEP 2: When I scroll down, I see a planner (9am-9pm); incldues  time, space to add notes/content + SAVE button
+
+// STEP 3: When viewing the timeblocks for such day, each timeblock is clor coded to indicate whether it's past, present, or future (gray = past, violent = current, blue = future)
+
+// STEP 4: When I click on a timeblock, I can enter an event.
+
+//STEP5: If I click the SAVE btn, then the event info is saved in local storage
+
+// STEP 6: When I refresh the page, the saved event is constantly displayed
